e304c2858304dc814598a920f27be5a098220bd8
Sihan2024 <sihan.zhu@bmw-brilliance.cn>
2024-08-25 21:05:04 +0800

    doc: update comment in bickshare.py and update gitignore

--- bikeshare.py
+++ bikeshare.py
@@ -121,13 +121,14 @@ def user_stats(df):
 def main():
     while True:
         city, month, day = get_filters()
+        # Load data by fiter of city ,month, day
         df = load_data(city, month, day)
 
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
+        # Recive Input
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
